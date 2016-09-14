aa36251fd6ffaec5f53e98e2c42959a30bd82546
Swalloow <joon9205@naver.com>
2016-09-14 18:42:27 +0900

    add news feed page

--- routes/feed/feed.js
+++ routes/feed/feed.js
@@ -1,14 +1,13 @@
+/**
+ * Created by Junyoung on 2016-09-14.
+ */
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET feed listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
-});
-
-/* GET feed page. */
-router.get('/:id/feed', function (req, res, next) {
-    res.render('feed');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get('/users', function (req, res, next) {
+    res.render('login');
+});
+
+router.get('/feed', function (req, res, next) {
+    res.render('feed');
 });
 
 module.exports = router;
