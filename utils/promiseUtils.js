2c2b0d99dd0347a7731bc9e5d6bbd082dcb3ef32
Swalloow <joon9205@naver.com>
2016-10-16 02:15:44 +0900

    fixed thumbnail size

--- utils/promiseUtils.js
+++ utils/promiseUtils.js
@@ -1,18 +1,18 @@
-// module.exports.defer = function() {
-//     return new Deferred();
-// }
-//
-// function Deferred() {
-//     var self = this;
-//     this.hasCanceled_ = false
-//     this.promise = new Promise(function(resolve, reject) {
-//         self.resolve = function(val) {
-//             self.hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
-//         }
-//         self.reject = reject
-//     })
-//
-//     this.cancel = function() {
-//         this.hasCanceled_ = true
-//     }
-// }
+module.exports.defer = function() {
+    return new Deferred();
+}
+
+function Deferred() {
+    var self = this;
+    this.hasCanceled_ = false
+    this.promise = new Promise(function(resolve, reject) {
+        self.resolve = function(val) {
+            self.hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
+        }
+        self.reject = reject
+    })
+
+    this.cancel = function() {
+        this.hasCanceled_ = true
+    }
+}
