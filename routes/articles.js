898849ab61a5a45aa928f3b6bdefe861da67ccff
phu54321 <phu54321@naver.com>
2016-10-19 05:32:39 +0900

    (HACK) articles/:id 가 렌더링되도록 했습니다.
    크게 2가지 수정사항이 있습니다.
    
    - Articles랑 Cluster.articles 사이의 _id가 일치하지 않기 때문에 /article 라우트의 링크를 따라
    들어간 /article/:id 에서 _id값이 Cluster 기준이라 제대로 렌더링이 되지 않았습니다. 임시로
    /article/:id에서도 Cluster 기준으로 하도록 해뒀습니다.
    
    - feeds에서 쓰는건 index.js가 아니라 articles.js에서 해결해야할 문제라 생각해 해당 라우트의 위치를
    옮겼습니다.

--- routes/articles.js
+++ routes/articles.js
@@ -3,10 +3,33 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
+var Cluster = mongoose.model('Cluster');
 
-/* GET home page. */
+// 뉴스 리스트
+router.get('/', function (req, res, next) {
+    // 각 클러스터마다 해당 클러스터에 포함된 뉴스들과 뉴스 갯수를 얻는다.
+    Cluster.aggregate([
+        { $sort : { "publishedAt": -1 } },
+        { $group : {
+            _id : "$cluster",
+            count: { $sum: 1 },
+            clusters: { $push: "$$ROOT" } }
+        },
+        { $project : {
+            count: 1,
+            clusters: { $slice: ["$clusters", 0, 1] }
+        } }
+    ],
+
+    function (err, clusters) {
+        if (err) res.send(err);
+        res.render('feed', {clusters: clusters});
+    });
+});
+
+
+// 각 뉴스마다
 router.get('/:id', function (req, res, next) {
-    Article.findOne({_id: req.params.id}, function (err, article) {
         if (err) throw err;
         res.render('article', {article: article});
     });
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Article = mongoose.model('Article');
-var Cluster = mongoose.model('Cluster');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -16,30 +15,7 @@ router.get('/login', function (req, res, next) {
     res.render('login');
 });
 
-/* GET articles page. */
-router.get('/articles', function (req, res, next) {
-    Cluster.aggregate([
-        { $sort : { "publishedAt": -1 } },
-        { $group : {
-            _id : "$cluster",
-            count: { $sum: 1 },
-            clusters: { $push: "$$ROOT" } }
-        },
-        { $project : {
-            count: 1,
-            clusters: { $slice: ["$clusters", 0, 1] }
-        } }
-    ],
-    function (err, clusters) {
-        if (err) res.send(err);
-        var values = [];
-        clusters.forEach(function (result) {
-            values.push(result.clusters);
-        });
-        res.render('feed', {clusters: clusters});
-        console.log(clusters);
-    });
-});
+
 
 /*
  계정이 있으면 로그인을 하고 없으면 새로 생성한다.
