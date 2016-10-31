f273204c174dbda551e5a53aa76a87aa16146ec1
phu54321 <phu54321@naver.com>
2016-10-31 11:22:10 +0900

    Browsersync와 nodemon을 연동해 빠른 개발을 가능하게 함

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
 
 // Lint Task
 gulp.task('lint', function () {
@@ -17,16 +18,34 @@ gulp.task('lint', function () {
 gulp.task('sass', function () {
     return gulp.src('public/stylesheets/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/stylesheets/'));
+        .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(browserSync.stream());
 });
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    return gulp.watch('public/**/*.scss', ['sass']);
+    gulp.watch('public/**/*.scss', ['sass']);
+    gulp.watch('views/**/*.jade', ['reload']);
 });
+
+
+var BROWSER_SYNC_RELOAD_DELAY = 2000;
 
 // Nodemon
 gulp.task('nodemon', function (cb) {
+    // Serve files from the root of this project
+
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["views/**/*.*, public/**/*.*"],
+        port: 7000
+    });
+
     var started = false;
     return nodemon({
         script: 'bin/www'
@@ -36,7 +55,14 @@ gulp.task('nodemon', function (cb) {
             cb();
             started = true;
         }
-    });
+    }).on('restart', function onRestart() {
+            // reload connected browsers after a slight delay
+            setTimeout(function reload() {
+                browserSync.reload({
+                    stream: false
+                });
+            }, BROWSER_SYNC_RELOAD_DELAY);
+        });
 });
 
 // Default Task
--- routes/users.js
+++ routes/users.js
@@ -18,5 +18,4 @@ router.get('/:id/feed', function (req, res) {
 });
 
 
-
 module.exports = router;
